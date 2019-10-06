71dba7a68f1b0e4cc3c2f58b2ee147a8dafaf752
moko <dlxy_moko@163.com>
2019-10-06 09:32:48 +0800

    完成列表页

--- meiduo_mall/meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/meiduo_mall/apps/goods/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
-from goods.views import ListView
+from goods.views import ListView, HotGoodsView
 
 urlpatterns = [
-    url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$', ListView.as_view(), name='list')
+    url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$', ListView.as_view(), name='list'),
+    url(r'^hot/(?P<category_id>\d+)/$', HotGoodsView.as_view(), name='hot'),
+
 ]
--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,13 +1,33 @@
 from django import http
+from django.core.paginator import Paginator, EmptyPage
 from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
 
 from contents.utils import get_categories
-from goods.models import GoodsCategory
+from goods.models import GoodsCategory, SKU
 from goods.utils import get_beradcrumb
+from meiduo_mall.utils.response_code import RETCODE
 
+
+class HotGoodsView(View):
+    '''热销排行商品'''
+    def get(self, request, category_id):
+        # 查询指定分类的sku，而且必须是上架商品，然后销量由高到底排序，最后进行切片
+        skus = SKU.objects.filter(category_id = category_id, is_launched=True).order_by('-sales')[:2]
+        # 构造json字典
+        hot_skus = []
+        for sku in skus:
+            sku_dict = {
+                'id': sku.id,
+                'name': sku.name,
+                'price': sku.price,
+                'default_image_url' : sku.default_image.url
+            }
+            hot_skus.append(sku_dict)
+
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg':"OK", 'hot_skus':hot_skus})
 
 class ListView(View):
     '''提供列表页'''
@@ -23,14 +43,43 @@ class ListView(View):
         # 面包屑导航
         beradcrumb = get_beradcrumb(category)
 
-
         # 查询商品分类
         categories = get_categories()
 
+        # 接收排序规则
+        sort = request.GET.get('sort', 'default')
+        # 按照排序规则查询该分类商品SKU信息
+        # 注意一定是模型字段里的
+        if sort == 'price':
+            sort_field = 'price'
+        elif sort == 'hot':
+            # 按销量降序
+            sort_field = '-sales'
+        else:
+            sort = 'default'
+            sort_field = 'create_time'
+        # 商品排序
+        skus = SKU.objects.filter(category=category, is_launched=True).order_by(sort_field)
+        # 商品分页，创建分页器
+        paginator = Paginator(skus, 5)      # 把skus分页，默认每页5条
+        # 获取当前用户要查看的那一页
+        try:
+            page_skus = paginator.page(page_num)
+        except EmptyPage:
+            # 如果page_num不正确，默认给用户404
+            return http.HttpResponseNotFound('empty page')
+        # 获取总页数
+        total_page = paginator.num_pages
+
         # 构造上下文
         context = {
-            'categories':categories,
-            'beradcrumb':beradcrumb,   # 面包屑导航
+            'categories' : categories,
+            'beradcrumb' : beradcrumb,   # 面包屑导航
+            'page_skus' : page_skus,     #当前页面的商品种类
+            'total_page' : total_page,   # 总页数
+            'page_num' : page_num,          # 当前页面
+            'sort' : sort,               # 排序规则
+            'category_id' : category_id, # 商品id
         }
 
         return render(request, 'list.html', context)
