4c84c478b47e4067f45622e8148ad80ecdb32d22
moko <dlxy_moko@163.com>
2019-10-07 10:08:26 +0800

    完成详情页

--- meiduo_mall/meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/meiduo_mall/apps/goods/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from goods.views import ListView, HotGoodsView
+from goods.views import ListView, HotGoodsView, DetailView
 
 urlpatterns = [
     url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$', ListView.as_view(), name='list'),
     url(r'^hot/(?P<category_id>\d+)/$', HotGoodsView.as_view(), name='hot'),
+    url(r'^detail/(?P<sku_id>\d+)/$', DetailView.as_view(), name='detail'),
 
 ]
--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -6,10 +6,35 @@ from django.shortcuts import render
 from django.views import View
 
 from contents.utils import get_categories
+from goods import models
 from goods.models import GoodsCategory, SKU
 from goods.utils import get_beradcrumb
 from meiduo_mall.utils.response_code import RETCODE
 
+
+class DetailView(View):
+    """商品详情页"""
+
+    def get(self, request, sku_id):
+        """提供商品详情页"""
+        # 获取当前sku的信息
+        try:
+            sku = models.SKU.objects.get(id=sku_id)
+        except models.SKU.DoesNotExist:
+            return render(request, '404.html')
+
+        # 查询商品频道分类
+        categories = get_categories()
+        # 查询面包屑导航
+        breadcrumb = get_beradcrumb(sku.category)
+
+        # 渲染页面
+        context = {
+            'categories': categories,
+            'breadcrumb': breadcrumb,
+            'sku': sku,
+        }
+        return render(request, 'detail.html', context=context)
 
 class HotGoodsView(View):
     '''热销排行商品'''
